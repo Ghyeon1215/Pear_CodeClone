7404d08c59caea93f0e81d28051ea98fb4f09f22
Hyeonyee <jgh10070@naver.com>
2020-11-12 15:25:26 +0900

    add router path

---
+++ Main_Gyuhyeon/Avocat_Pear/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ReportWriting } from "./ReportWriting/ReportWriting";
+export { default as ReportWritingModal } from "./Modal/ReportWritingModal";
+export { default as TeamStateModal } from "./Modal/TeamStateModal";
