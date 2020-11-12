22b022a3f65016c95fcef51ea0fa5405f263aa77
Hyeonyee <jgh10070@naver.com>
2020-11-12 15:25:40 +0900

    add & modified router

--- Main_Gyuhyeon/Avocat_Pear/src/App.js
+++ Main_Gyuhyeon/Avocat_Pear/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import GlobalFonts from "./GlobalStyle";
-import ReportWriting from "./Components/ReportWriting/ReportWriting";
+import UserRouter from "./Routers/UserRouter";
+
 
 function App() {
   return (
     <>
       <GlobalFonts />
-      <Router>
-        <Switch>
-        <Route path="/report-writing" component={ReportWriting} />
-          {/* uri 명명 규칙은 카멜이나 스네이크등의 케이스보단 케밥 케이스를 선호 */}        </Switch>
-      </Router>
+      <BrowserRouter>
+        <Route path="/" component={UserRouter} />
+      </BrowserRouter>
     </>
   );
 }
