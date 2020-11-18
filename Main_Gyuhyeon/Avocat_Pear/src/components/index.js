dbd3420c116a4d80bb98a96e7616692f3592bb8c
Hyeonyee <jgh10070@naver.com>
2020-11-18 23:56:53 +0900

    ㅁㅇㅇ

--- Main_Gyuhyeon/Avocat_Pear/src/components/index.js
+++ Main_Gyuhyeon/Avocat_Pear/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as ReportWriting } from "./ReportWriting/ReportWriting";
 export { default as ReportWritingModal } from "./Modal/ReportWritingModal";
 export { default as TeamStateModal } from "./Modal/TeamStateModal";
+export { default as SubmitReportModal } from "./Modal/SubmitReportModal";
 export { default as SubmitSuccess } from "./Modal/SubmitSuccess";
