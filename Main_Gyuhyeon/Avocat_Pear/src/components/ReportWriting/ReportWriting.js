2e0bf0994fa7ed31e9bb24b289551633e71c4f01
Hyeonyee <jgh10070@naver.com>
2020-11-20 23:44:18 +0900

    add code

--- Main_Gyuhyeon/Avocat_Pear/src/components/ReportWriting/ReportWriting.js
+++ Main_Gyuhyeon/Avocat_Pear/src/components/ReportWriting/ReportWriting.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import SubmitReportModal from '../Modal/SubmitReportModal';
 import * as S from '../styled/ReportWriting/style';
 // In의 약자 I
 import * as I from '../styled/ReportWriting/InStyle'; 
@@ -10,6 +11,7 @@ import { link } from "../../assets";
 
 const ReportWriting = () => {
     const [hoverNumber, setHoverNumber] = useState(0)
+    const [ state, setState ] = useState(false);
 
     const onMouseOver = (e) => {
         setHoverNumber(Number(e.currentTarget.dataset.id))
@@ -17,18 +19,25 @@ const ReportWriting = () => {
     const onMouseLeave = () => { 
         setHoverNumber(0)
     }
+    const onClick = () => {
+        setState(true);
+    }
+
     return (
+        <>
+        {
+            state === true ? 
+            <SubmitReportModal setState={setState}/>
+            : null
+        }
         <S.Main>
-            {/* margin 맥여주는 div */}
+            
             <S.BorderBox> 
-                {/* 안쪽 padding 맥여주는 div */}
                <S.InlineBox>
                     <S.ReportLogo> 
-                        {/* <보고서 작성하기 img> 맥여주는 span */}
                         <span><img src={RWlogo} alt="Rwlogo"/></span>
                     </S.ReportLogo>
                     <S.SelectBoxs>
-                        {/* 사용 언어, 학년 등 선택박스 만드는 div */}
                         <S.PaddingBox>
                         { /*<S.Select> 
                         
@@ -82,65 +91,55 @@ const ReportWriting = () => {
                             </S.Select>
                         </S.PaddingBox>
                     </S.SelectBoxs>
-                    {/* 보고서 제목 ~ 보고서 파일 첨부까지 */}
                     <S.ReportMain>
-                        {/* 보고서 제목 */}
                         <S.ReportHeader>
                             <input type={Text} placeholder="개발 보고서의 제목을 입력해주세요." />
                         </S.ReportHeader>
-                        {/* 보고서 소개 글 */}
                         <S.ReprotWriteBox>
                             <textarea name="writingbox" rows="15" cols="40" minLength="10" placeholder="팀이 작성한 개발보고서에 대한 소개글을 입력해주세요." style={{resize:"none"}}></textarea>
                         </S.ReprotWriteBox>
-                        {/* 팀 깃헙 링크 */}
                         <S.LinkBox>
                             <span>
-                                <form name="githubLink" action="" method="post" autoComplete="on">
+                                <div>
                                     <img src={link} alt="gitgub-link"/><input type={Text} placeholder="팀의 GITHUB 링크를 입력해주세요 (선택)" />
-                                </form>
+                                </div>
                             </span>
                         </S.LinkBox>
-                        {/* 보고서 파일 첨부 */}
                         <S.AttachFile>
                             <span><img src={link} alt="attachfile"/></span>
                         </S.AttachFile>
                     </S.ReportMain>
-                    {/* 팀 제작 ~ submit btn */}
                     <S.SubmitBox>
-                        {/* 팀 제작 (left) */}
                         <S.MakeTeam>
-                            {/* add teamName */}
-                            <form name="makeTeam" action="" method="post">
+                            <span>
                                 <I.TeamLeftBox>
                                     <I.TeamName>
                                         <span><input type={Text} placeholder="팀의 이름을 입력해주세요." /></span>
                                     </I.TeamName>
-                                    {/* add member */}
                                     <I.TeamMember>
                                         <span><input type={Text} placeholder="팀원을 입력해주세요."/><img src={searchImg} alt="inputmember"/>
                                         </span>
                                     </I.TeamMember>
                                 </I.TeamLeftBox>
                                 <I.TeamRightBox>
-                                    <button type="submit">팀 만들기</button>
+                                    <div>팀 만들기</div>
                                 </I.TeamRightBox>
-                            </form>
+                            </span>
                         </S.MakeTeam>
                         {/* save & submit btn (right) */}
                         <S.SaveSubBtn>
                             <I.SaveBtn>
-                                <button>임시저장</button>
+                                <div>임시저장</div>
                             </I.SaveBtn>
-                            <I.SubBtn>
-                                <form name="reportSubmit" action="" method="post">
-                                    <button type="submit">제출하기</button>
-                                </form>
+                            <I.SubBtn onClick={onClick}>
+                                <div>제출하기</div>
                             </I.SubBtn>
                         </S.SaveSubBtn>
                     </S.SubmitBox>
                </S.InlineBox>
             </S.BorderBox>
         </S.Main>
+        </>
     );
 }
 
